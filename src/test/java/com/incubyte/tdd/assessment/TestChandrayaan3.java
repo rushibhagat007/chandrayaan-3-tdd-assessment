8d2d673b75241f5f8f158378c71c76db56c964bc
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-27 10:46:33 +0530

    02 - Added first test case for testing chandrayan

--- src/test/java/com/incubyte/tdd/assessment/TestChandrayaan3.java
+++ src/test/java/com/incubyte/tdd/assessment/TestChandrayaan3.java
@@ -1,6 +1,14 @@
 package com.incubyte.tdd.assessment;
 
+import org.junit.Test;
 
 public class TestChandrayaan3 {
+	
+	@Test
+    public void testInitialPositionAndDirection() {
+        Chandrayaan3 c = new Chandrayaan3(0, 0, 0, 'N');
+        assertEquals("(0, 0, 0)", c.getPosition());
+        assertEquals('N', c.getDirection());
+    }
 
 }
