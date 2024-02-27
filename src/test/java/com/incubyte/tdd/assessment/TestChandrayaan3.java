998020e7d0669987ca65ee89c700235061d81f54
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-27 11:21:54 +0530

    04 - Added testMoveForward method in TestChandrayaan3 class

--- src/test/java/com/incubyte/tdd/assessment/TestChandrayaan3.java
+++ src/test/java/com/incubyte/tdd/assessment/TestChandrayaan3.java
@@ -5,12 +5,19 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestChandrayaan3 {
-	
+
 	@Test
-    public void testInitialPositionAndDirection() {
-        Chandrayaan3 c = new Chandrayaan3(0, 0, 0, 'N');
-        assertEquals("(0, 0, 0)", c.getPosition());
-        assertEquals('N', c.getDirection());
-    }
+	public void testInitialPositionAndDirection() {
+		Chandrayaan3 c = new Chandrayaan3(0, 0, 0, 'N');
+		assertEquals("(0, 0, 0)", c.getPosition());
+		assertEquals('N', c.getDirection());
+	}
+
+	@Test
+	public void testMoveForward() {
+		Chandrayaan3 c = new Chandrayaan3(0, 0, 0, 'N');
+		c.moveForward();
+		assertEquals("(0, 1, 0)", c.getPosition());
+	}
 
 }
